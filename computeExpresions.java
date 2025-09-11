c1b0cdf21b4656677a0fb183c724e05b4e3f5f66
Blaise202 <iblaise2023@gmail.com>
2025-09-11 14:38:19 +0000

    math expressions

---
+++ computeExpresions.java
@@ -0,0 +1,10 @@
+public class computeExpresions {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        int product = a * b;
+        System.out.print("Sum: " + sum);
+        System.out.println("Product: " + product);
+    }
+}
