f34ec63d9b6514a8a4697ee3785b8a33496458b2
Blaise202 <iblaise2023@gmail.com>
2025-09-23 10:42:59 +0000

    Integer division

---
+++ IntDivision.java
@@ -0,0 +1,9 @@
+public class IntDivision {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 2;
+    double result = (double) a / b; // normal` division
+    System.out.println("Result of normal division: " + result); // Outputs 2.5
+    System.out.println("Random number: " + Math.random());
+  }
+}
