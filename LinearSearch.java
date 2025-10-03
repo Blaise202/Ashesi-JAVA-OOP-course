d8e610d451ad61e5c02ff48e068601370171ce5c
Blaise202 <iblaise2023@gmail.com>
2025-10-03 13:03:02 +0000

    Linear Search - Swapping the indices

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class LinearSearch{
   public static void main(String[] args){
     int[] numbers = {2,5,8,3,4,6,10,15,20,1,11};
+
+    // Task 1 finding value of index provided by the user. 
+
     Scanner scanner = new Scanner(System.in);
     int indexToFind, value = 0;
     int limit = numbers.length-1;
@@ -12,7 +15,17 @@ public class LinearSearch{
         value = numbers[i];
       }
     }
-    
     System.out.println("The number is: "+ value);
+    
+    // Task 2 Swaping the index searched with it's next value;
+    
+    System.out.println("Let's switch value on index "+indexToFind+"("+numbers[indexToFind]+") and on index "+(indexToFind+1)+"("+numbers[indexToFind+1]+")");
+
+    int reserve = numbers[indexToFind];
+    numbers[indexToFind] = numbers[indexToFind+1];
+    numbers[indexToFind+1] = reserve;
+
+    System.out.println("Now the values changed to index "+indexToFind+"("+numbers[indexToFind]+") and index "+(indexToFind+1)+"("+numbers[indexToFind+1]+")");
+
   }
 }
