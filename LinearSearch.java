a8318d9c766eb45a86718b8f9d4dd96cd1723a7b
Blaise202 <iblaise2023@gmail.com>
2025-10-03 13:40:07 +0000

    Linear Search - Swapping the indices

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class LinearSearch{
   public static void main(String[] args){
     int[] numbers = {2,5,8,3,4,6,10,15,20,1,11};
@@ -20,12 +21,27 @@ public class LinearSearch{
     // Task 2 Swaping the index searched with it's next value;
     
     System.out.println("Let's switch value on index "+indexToFind+"("+numbers[indexToFind]+") and on index "+(indexToFind+1)+"("+numbers[indexToFind+1]+")");
-
+    
     int reserve = numbers[indexToFind];
     numbers[indexToFind] = numbers[indexToFind+1];
     numbers[indexToFind+1] = reserve;
 
     System.out.println("Now the values changed to index "+indexToFind+"("+numbers[indexToFind]+") and index "+(indexToFind+1)+"("+numbers[indexToFind+1]+")");
+    
+    // Task 3 - Sorting array in ascending and descending order.
 
+    int[] ascending = numbers;
+
+    System.out.println("Before sort: "+Arrays.toString(ascending));
+
+    for(int j = 0; j < ascending.length; j++){
+      if(ascending.length > (j+1) && ascending[j] > ascending[j+1]){
+        reserve = ascending[j];
+        ascending[j] = ascending[j+1];
+        ascending[j+1] = reserve;
+        j = 0;
+      }
+    }
+    System.out.println("After sort: "+Arrays.toString(ascending));
   }
 }
---
+++ Test.java
@@ -0,0 +1,15 @@
+puclic class Test{
+  public static void main(String[] args){
+    System.out.println("Sorting the array in ascending order...");
+    // Bubble sort implementation (ascending order)
+    for (int i = 0; i < ascending.length - 1; i++) {
+      for (int j = 0; j < ascending.length - i - 1; j++) {
+        if (ascending[j] > ascending[j + 1]) {
+          int temp = ascending[j];
+          ascending[j] = ascending[j + 1];
+          ascending[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
