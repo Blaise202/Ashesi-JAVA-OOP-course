f94216d7a01436bfb726fe865369aa054c12180c
Blaise202 <iblaise2023@gmail.com>
2025-10-04 10:45:41 +0000

    added code on test.java

--- Test.java
+++ Test.java
@@ -11,5 +11,16 @@ puclic class Test{
         }
       }
     }
+    System.out.println("Sorting the array in descending order...");
+    // Bubble sort implementation (descending order)
+    for (int i = 0; i < ascending.length - 1; i++) {
+      for (int j = 0; j < ascending.length - i - 1; j++) {
+        if (ascending[j] < ascending[j + 1]) {
+          int temp = ascending[j];
+          ascending[j] = ascending[j + 1];
+          ascending[j + 1] = temp;
+        }
+      }
+    }
   }
 }
