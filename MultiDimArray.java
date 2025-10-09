492ccc07d8bdbf3f108927a913453745aca0eeed
Blaise202 <iblaise2023@gmail.com>
2025-10-09 12:19:54 +0000

    multi dimention

---
+++ MultiDimArray.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+public class MultiDimArray{
+
+  public String name = "Blaise";
+
+  public static String PrintName(String name){
+    // this.name = name;
+    return name;
+  }
+
+  public static void main(String[] args){
+    int[][] numbers = {
+      {2,3,4,5,5,6},
+      {3,4,5,6,7,8},
+      {2,5,6,3,4,5},
+      {6,4,3,4,5,6}
+    };
+    for(int[] part : numbers){
+      for(int num: part){
+        System.out.print(num+" ");
+      }
+      System.out.print("\n");
+    }
+    int[][][] threeDim = {
+      {
+        {1,2,3,4,5},
+        {3,4,5,2,3},
+        {3,4,2,3,5}
+      },
+      {
+        {2,3,5,3,4},
+        {7,6,5,6,7},
+        {4,5,6,4,3}
+      },
+      {
+        {2,3,4,5,6},
+        {2,3,4,4,5}
+      }
+    };
+
+    for(int[][] twoDim : threeDim){
+      for(int[] oneDim : twoDim){
+        for(int num : oneDim){
+          System.out.print("");
+        }
+      }
+    }
+
+    System.out.print(PrintName("Remy"));
+
+  }
+}
--- Test.java
+++ Test.java
@@ -1,26 +1,8 @@
-puclic class Test{
+package MultiDimArray;
+import MultiDimArray.MultiDimArray;
+public class Test{
   public static void main(String[] args){
-    System.out.println("Sorting the array in ascending order...");
-    // Bubble sort implementation (ascending order)
-    for (int i = 0; i < ascending.length - 1; i++) {
-      for (int j = 0; j < ascending.length - i - 1; j++) {
-        if (ascending[j] > ascending[j + 1]) {
-          int temp = ascending[j];
-          ascending[j] = ascending[j + 1];
-          ascending[j + 1] = temp;
-        }
-      }
-    }
-    System.out.println("Sorting the array in descending order...");
-    // Bubble sort implementation (descending order)
-    for (int i = 0; i < ascending.length - 1; i++) {
-      for (int j = 0; j < ascending.length - i - 1; j++) {
-        if (ascending[j] < ascending[j + 1]) {
-          int temp = ascending[j];
-          ascending[j] = ascending[j + 1];
-          ascending[j + 1] = temp;
-        }
-      }
-    }
+    MultiDimArray multi = new MultiDimArray();
+    System.out.print(multi.PrintName("me"));
   }
 }
