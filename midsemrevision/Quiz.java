6689584c5fd1c0bfe2978bd08be05aa6a6956a11
Blaise202 <iblaise2023@gmail.com>
2025-10-23 16:56:16 +0000

    Rev - Quiz

---
+++ midsemrevision/Quiz.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Quiz{
+  public static void main(String[] args){
+    int number1 = (int) (Math.random() * 10);
+    int number2 = (int) (Math.random() * 10);
+    System.out.print("What is "+number1+" - "+number2+" : ");
+    Scanner scanner = new Scanner(System.in);
+    int answer = scanner.nextInt();
+    if(number1 - number2 == answer){
+      System.out.println("🥳Correct!");
+    }else{
+      System.out.println("😢Wrong!");
+    }
+  }
+}
