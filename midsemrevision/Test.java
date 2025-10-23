02b27f7d7f2f8534bd1520af1674f9244dde9d67
Blaise202 <iblaise2023@gmail.com>
2025-10-23 16:25:29 +0000

    Test - Nested If

--- midsemrevision/Test.java
+++ midsemrevision/Test.java
@@ -1,5 +1,11 @@
 public class Test{
   public static void main(String[] args){
     System.out.println(Math.PI); // it's Math.PI not just PI
+
+    int number = (int) (Math.random() * 10);
+    if(number%2 == 0)
+      if(number > 0)
+        System.out.println(number + " is even");
+    System.out.println(number + " is odd");
   }
 }
